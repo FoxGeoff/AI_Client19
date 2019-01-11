78520c6b452e501de03901af61fcca63f0a6f698
FoxGeoff <fox.geoffrey@gmail.com>
2019-01-11 10:42:08 -0800

    Initial Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule}from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+ 
+
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: './contactmanager/contactmanager.module#ContactmanagerModule'},
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule'}, 
+  { path: '**', redirectTo: 'contactmanager'},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
