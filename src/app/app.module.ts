18ff94e4ab3b6ec44d7533b5477244daa695290e
FoxGeoff <fox.geoffrey@gmail.com>
2019-01-12 18:22:40 -0800

    Update dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from './shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
+  {path: '', component: DashboardComponent},
   { path: 'contactmanager', loadChildren: './contactmanager/contactmanager.module#ContactmanagerModule' },
   { path: 'customermanager', loadChildren: './customermanager/customermanager.module#CustomermanagerModule' },
   { path: 'demo', loadChildren: './demo/demo.module#DemoModule' },
-  { path: '**', redirectTo: 'customermanager' },
+  { path: '**', redirectTo: '' },
 ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
