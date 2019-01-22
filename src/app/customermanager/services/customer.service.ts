3707f1e0e58cfb75515053e895b4116765ac861a
FoxGeoff <fox.geoffrey@gmail.com>
2019-01-21 18:29:20 -0800

    refactored LoadAll()

--- src/app/customermanager/services/customer.service.ts
+++ src/app/customermanager/services/customer.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { Customer } from '../models/customer';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { userInfo } from 'os';
+import { BookTrackerError } from 'src/app/models/bookTrackerError';
+import { catchError } from 'rxjs/operators';
+import { CustomerTrackerError } from '../models/CustomerTrackerError';
 
 @Injectable({
   providedIn: 'root'
@@ -58,20 +61,41 @@ export class CustomerService {
     });
   }
 
-  LoadAll() {
-    const userUrl = 'https://localhost:44334/api/customers';
-
-    return this.https.get<Customer[]>(userUrl)
-      .subscribe(data => {
+  LoadAll(): void {
+    this.getAllCustomers().subscribe(
+      (data: Customer[]) => {
+        console.log(data);
         this.dataStore.customers = data;
         // Copy data obj to isolate the data from manipulation
         // and expose this data
         this._customers.next(Object.assign({}, this.dataStore).customers);
-        console.log("Got customer data");
-      }, error => {
-        console.error("Failed to fetch customer data");
-      }
-      )
+      },
+      (err: CustomerTrackerError) => console.log(err.friendlyMessage),
+      () => console.log('Finished getting customer data.')
+    );
+    this._customers.next(Object.assign({}, this.dataStore).customers);
+  }
+
+  //move to: data service
+  getAllCustomers(): Observable<Customer[] | CustomerTrackerError> {
+    const userUrl = 'https://localhost:44334/api/customers';
+
+    console.log('Getting all customers from the server');
+    // Test: '/api/error/500'
+    return this.https.get<Customer[]>(userUrl)
+      .pipe(
+        catchError(err => this.handleHttpError(err))
+      );
+  }
+
+  //move to: data service
+  private handleHttpError(error: HttpErrorResponse): Observable<CustomerTrackerError> {
+    let dataError = new CustomerTrackerError();
+    dataError.errorNumber = 100;
+    dataError.message = error.statusText;
+    dataError.friendlyMessage = 'An error occured retriving customer data.';
+
+    return throwError(dataError);
   }
 
   customerById(id: number): Customer {
