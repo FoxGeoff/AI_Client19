dffc25a90d9c13260fd54c6c98df4c5e9bb2e000
FoxGeoff <fox.geoffrey@gmail.com>
2019-02-04 12:07:11 -0800

    Add: Code Invoicemanager/MainContent

--- src/app/invoicemanager/components/main-content/main-content.component.ts
+++ src/app/invoicemanager/components/main-content/main-content.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/customermanager/models/customer';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from 'src/app/customermanager/services/customer.service';
+import { CustomerParameterService } from 'src/app/customermanager/services/customer-parameter.service';
 
 @Component({
   selector: 'app-main-content',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+  customer: Customer;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private service: CustomerService,
+    private customerParameterService: CustomerParameterService) { }
 
   ngOnInit() {
-  }
+    this.route.params.subscribe(params => {
+      let id = params['id'];
+      if (!id) id = 11;
+      this.customer = null;;
 
+      this.service.customers.subscribe(customers => {
+        if (customers.length == 0) return;
+
+        setTimeout(() => {
+          this.customer = this.service.customerById(id);
+          this.customerParameterService.detailedCustomer = this.customer;
+        }, 500);
+      });
+    });
+  }
 }
+
