d2c34df5802ad18d25252bf0475a3527eeee17bf
FoxGeoff <fox.geoffrey@gmail.com>
2019-03-11 14:33:38 -0700

    Add component files

--- src/app/invoice-productmanager/invoice-productmanager.module.ts
+++ src/app/invoice-productmanager/invoice-productmanager.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvoiceProductmanagerAppComponent } from './invoice-productmanager-app.component';
+import { SidenavComponent } from  './../invoice-productmanager/component/sidenav/sidenav.component';
+import { ToolbarComponent } from  './../invoice-productmanager/component/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [InvoiceProductmanagerAppComponent],
+  declarations: [InvoiceProductmanagerAppComponent, SidenavComponent, ToolbarComponent],
   imports: [
     CommonModule
   ]
