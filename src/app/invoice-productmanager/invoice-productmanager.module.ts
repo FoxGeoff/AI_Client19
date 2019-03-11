e174ddec559ed79223f9e1ea5096ebe3e0e50a90
FoxGeoff <fox.geoffrey@gmail.com>
2019-03-10 17:38:46 -0700

    Add files invoice-productmanager

---
+++ src/app/invoice-productmanager/invoice-productmanager.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InvoiceProductmanagerAppComponent } from './invoice-productmanager-app.component';
+
+@NgModule({
+  declarations: [InvoiceProductmanagerAppComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class InvoiceProductmanagerModule { }
