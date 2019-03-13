8672b11b77016691bb003fbefa5d1b59256e9148
FoxGeoff <fox.geoffrey@gmail.com>
2019-03-12 17:48:28 -0700

    Add Product Manager Layout

--- src/app/productmanager/component/sidenav/sidenav.component.ts
+++ src/app/productmanager/component/sidenav/sidenav.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatDrawer } from '@angular/material';
+import { Product } from '../../models/Product';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +12,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  smallWidthBreakpoint: boolean;
+  Products: Observable<Product[]>;
+  @ViewChild(MatDrawer) sidenav: MatDrawer;
 
-  constructor() { }
+  
+  constructor(
+    private breakpointObserver: BreakpointObserver, 
+    private ProductService: ProductService, 
+    private router: Router) { }
 
   ngOnInit() {
+     // make layout responsive
+     this.breakpointObserver
+     .observe(['(min-width: 500px)'])
+     .subscribe((state: BreakpointState) => {
+       if (state.matches) {
+         console.log('Viewport is 500px or over!');
+         this.smallWidthBreakpoint = false;
+       } else {
+         console.log('Viewport is getting smaller!');
+         this.smallWidthBreakpoint = true;
+       }
+     });
+  }
+
+
+  isScreenSmall(): boolean {
+    return this.smallWidthBreakpoint;
   }
 
 }
