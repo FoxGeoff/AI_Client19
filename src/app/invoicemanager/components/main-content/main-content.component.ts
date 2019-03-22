314e42dd4d10074c36d407482075173c01d6660d
FoxGeoff <fox.geoffrey@gmail.com>
2019-03-22 16:33:10 -0700

    Add invoice text and spinner

--- src/app/invoicemanager/components/main-content/main-content.component.ts
+++ src/app/invoicemanager/components/main-content/main-content.component.ts
@@ -3,7 +3,9 @@ import { Invoice } from 'src/app/invoicemanager/models/invoice';
 import { ActivatedRoute } from '@angular/router';
 import { InvoiceService } from 'src/app/invoicemanager/services/invoice.service';
 import { InvoiceParameterService } from '../../services/invoice-parameter.service';
- 
+import { isNull } from 'util';
+import { NullViewportScroller } from '@angular/common/src/viewport_scroller';
+
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
@@ -11,23 +13,24 @@ import { InvoiceParameterService } from '../../services/invoice-parameter.servic
 })
 export class MainContentComponent implements OnInit {
   invoice: Invoice;
+  id: number;
 
   constructor(private route: ActivatedRoute,
     private service: InvoiceService,
-    private customerParameterService: InvoiceParameterService) { }
+    private invoiceParameterService: InvoiceParameterService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      let id = params['id'];
-      if (!id) id = 11;
-      this.invoice = null;;
+      this.id = (params['id']) || null;
+      this.invoice = null;
 
       this.service.invoices.subscribe(invoices => {
         if (invoices.length == 0) return;
-
+    
         setTimeout(() => {
-          this.invoice = this.service.invoiceById(id);
-          this.customerParameterService.detailedInvoice = this.invoice;
+          console.log(this.id);
+          this.invoice = this.service.invoiceById(this.id);
+          this.invoiceParameterService.detailedInvoice = this.invoice;
         }, 500);
       });
     });
